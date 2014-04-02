693de2595675ea282a3ca4d03cffb4fc990ff263
crazypoo <273277355@qq.com>
2014-04-03 01:08:09 +0800

    修復按鈕BUG

--- PooAlerView/PooViewController.m
+++ PooAlerView/PooViewController.m
@@ -81,7 +81,7 @@
 {
     [self.inputCode resignFirstResponder];
     
-    __block int timeout = 60;
+    __block int timeout = 3;
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
     dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0);
@@ -90,6 +90,7 @@
             dispatch_source_cancel(_timer);
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.otherButton setTitle:@"有碼！" forState:UIControlStateNormal];
+                self.otherButton.userInteractionEnabled = YES;
                 [self.otherButton addTarget:self action:@selector(alertViewbottomButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
             });
         }
@@ -100,6 +101,7 @@
             NSString *strTime = [NSString stringWithFormat:@"%d分%.2d秒",minutes, seconds];
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.otherButton setTitle:strTime forState:UIControlStateNormal];
+                self.otherButton.userInteractionEnabled = NO;
             });
             timeout--;
             
