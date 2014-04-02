746812fcbec15e70c9854e0d76ec42cf9bd08d27
crazypoo <273277355@qq.com>
2014-04-02 20:54:37 +0800

    Initial Commit

---
+++ PooAlerView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PooAlerView
+//
+//  Created by crazypoo on 14-4-2.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PooAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PooAppDelegate class]));
+    }
+}
